f6c89e950eabcf9a4359a24981963cb2498683b0
Saket37 <anandsaket37@gmail.com>
2025-06-25 00:52:39 +0530

    impl apps list viewmodel

--- app/src/main/java/com/example/dynadroid/di/ProvideViewModel.kt
+++ app/src/main/java/com/example/dynadroid/di/ProvideViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.dynadroid.di
 
+import com.example.dynadroid.ui.installed_apps_list.InstalledAppsListViewModel
 import com.example.dynadroid.ui.on_boarding.select_apps.AppSelectionViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::AppSelectionViewModel)
+    viewModelOf(::InstalledAppsListViewModel)
 }
--- app/src/main/java/com/example/dynadroid/ui/installed_apps_list/InstalledAppsListViewModel.kt
+++ app/src/main/java/com/example/dynadroid/ui/installed_apps_list/InstalledAppsListViewModel.kt
@@ -1,4 +1,45 @@
 package com.example.dynadroid.ui.installed_apps_list
 
-class InstalledAppsListViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dynadroid.data.model.AppInfo
+import com.example.dynadroid.domain.InstalledDeviceApps
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+data class AppListState(
+    val isLoading: Boolean = true,
+    val apps: List<AppInfo> = emptyList(),
+    val errorMessage: String? = null
+)
+
+class InstalledAppsListViewModel(private val deviceApps: InstalledDeviceApps) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(AppListState())
+    val uiState: StateFlow<AppListState> = _uiState
+
+    init {
+        loadApps()
+    }
+
+    private fun loadApps() {
+        viewModelScope.launch {
+            deviceApps.getInstalledApps()
+                .onStart { _uiState.value = AppListState(isLoading = true) }
+                .catch { e ->
+                    _uiState.value = AppListState(
+                        isLoading = false,
+                        apps = emptyList(),
+                        errorMessage = e.localizedMessage ?: "Unknown Error"
+                    )
+                }
+                .collect { apps ->
+                    _uiState.value = AppListState(isLoading = false, apps = apps)
+                }
+        }
+    }
+
 }
