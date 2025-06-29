76063f84e7c3760bb42abb37f82ed69ba5f7081d
Saket37 <anandsaket37@gmail.com>
2025-06-29 12:07:26 +0530

    migrated schema location from ksp to room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 android {
@@ -18,10 +19,10 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
-    ksp {
-        arg("room.schemaLocation", "$projectDir/schemas")
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
+
 
     buildTypes {
         release {
